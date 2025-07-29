24052385d4801f93afa64e0b7b7c22e14d6510dd
Эрдэнэхүү Энхээ <erdenekhuu.e@gmobile.mn>
2025-07-29 09:50:42 +0800

    sample

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello, I am Jarvis")
+import os
+from module.nn import NeuralNetwork
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+def main():
+    device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+    model = NeuralNetwork().to(device)
+    print(model)
+    
+
+if __name__ == "__main__":
+    main()
